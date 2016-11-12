c5404ca5ef31a079c40ef2e83568f20fb72dbebf
TRACE-PC\Trace <tracerainbolt>
2016-11-11 19:37:10 -0800

    Added clouds.

---
+++ Assets/Scripts/CloudGenerator.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudGenerator : MonoBehaviour {
+    public GameObject[] cloud;
+
+	// Use this for initialization
+	void Start () {
+        placeClouds();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void placeClouds()
+    {
+        for (int i = 0; i <= 3; i++)
+        {
+            for (int j = 0; j < 200; j++)
+            {
+                Instantiate(cloud[i], new Vector3(Random.Range(-50f, 500f), (i - 1)*6 + 2.2f + Random.Range(-0.92f, 0.82f), 0.2f), Quaternion.identity);
+            }
+        }
+    }
+}
