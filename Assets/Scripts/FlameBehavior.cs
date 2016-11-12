95098108bd21369e5498eeceb095058178b9b0fe
NathanPhilliber <nphilliber@gmail.com>
2016-11-12 15:48:23 -0800

    Added flame thrower

---
+++ Assets/Scripts/FlameBehavior.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlameBehavior : MonoBehaviour {
+
+
+	public float power;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void Fire(Rigidbody playerRb, bool isRight){
+		
+		Vector3 yV = Vector3.zero;
+		if (Random.Range (0, 2) == 0) {
+			yV = Vector3.up;
+		} else {
+			yV = Vector3.down;
+		}
+
+
+		Vector3 move = new Vector3 (power, Random.Range(-1.1f,1.1f),0);
+		if (!isRight) {
+			move = new Vector3 (-power, Random.Range(-1.1f,1.1f),0);
+		}
+
+		GetComponent<Rigidbody> ().velocity = playerRb.velocity + move;
+
+		Destroy (gameObject, Random.Range(.4f,.8f));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/FlameThrowerBehavior.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlameThrowerBehavior : MonoBehaviour {
+
+	public GameObject flame;
+	public Rigidbody playerRb;
+	public PlayerController player;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (player.dimension == WorldControlManager.enabledWorld) {
+			if (Input.GetKey (KeyCode.RightArrow)) {
+				GameObject spawned = (GameObject)Instantiate (flame, transform.position, Quaternion.identity);
+				spawned.GetComponent<FlameBehavior> ().Fire (playerRb, true);
+			}
+			if (Input.GetKey (KeyCode.LeftArrow)) {
+				GameObject spawned = (GameObject)Instantiate (flame, transform.position, Quaternion.identity);
+				spawned.GetComponent<FlameBehavior> ().Fire (playerRb, false);
+			}
+		}
+	}
+}
