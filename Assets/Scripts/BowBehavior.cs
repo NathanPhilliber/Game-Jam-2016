2cffbea4e2c922477aceb89b0f204e1ac2503d27
TRACE-PC\Trace <tracerainbolt>
2016-11-12 18:43:47 -0800

    Fixed merge

--- Assets/Scripts/BowBehavior.cs
+++ Assets/Scripts/BowBehavior.cs
@@ -1,42 +1,71 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BowBehavior : MonoBehaviour {
+public class BowBehavior : MonoBehaviour
+{
 
-	public GameObject arrow;
-	public bool isRight;
-	public PlayerController player;
-	public int maxChargeTime;
-	private int charge;
+    public GameObject arrow;
+    public bool isRight;
+    public PlayerController player;
+    public int maxChargeTime;
+    private int charge;
 
-	public AudioClip shotSound;
-	private AudioSource source;
+    public AudioClip shotSound;
+    private AudioSource source;
 
-	// Use this for initialization
-	void Start () {
-		source = GetComponent<AudioSource> ();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (player.dimension == WorldControlManager.enabledWorld) {
-			if (isRight && Input.GetKey (KeyCode.RightArrow)) {
-				charge++;
-				if (charge >= maxChargeTime) {
-					Instantiate (arrow, transform.position, Quaternion.identity);
-					charge = 0;
-					source.PlayOneShot (shotSound);
-				}
-			}
-			if (!isRight && Input.GetKey (KeyCode.LeftArrow) ) {
-				
-				charge++;
-				if (charge >= maxChargeTime) {
-					Instantiate (arrow, transform.position, Quaternion.identity);
-					charge = 0;
-					source.PlayOneShot (shotSound);
-				}
-			}
-		}
-	}
+    private Animator anim;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+        source = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.dimension == WorldControlManager.enabledWorld)
+        {
+            if (isRight && Input.GetKey(KeyCode.RightArrow))
+            {
+                charge++;
+                if (charge >= maxChargeTime - 20)
+                {
+                    anim.SetBool("isFiring", true);
+                }
+
+                if (charge >= maxChargeTime)
+                {
+                    Instantiate(arrow, transform.position, Quaternion.identity);
+                    charge = 0;
+                    source.PlayOneShot(shotSound);
+                    anim.SetBool("isFiring", false);
+                }
+            }
+            if (!isRight && Input.GetKey(KeyCode.LeftArrow))
+            {
+                charge++;
+
+                if (charge >= maxChargeTime - 20)
+                {
+                    anim.SetBool("isFiring", true);
+                }
+
+                if (charge >= maxChargeTime)
+                {
+                    Instantiate(arrow, transform.position, Quaternion.identity);
+                    charge = 0;
+                    source.PlayOneShot(shotSound);
+                    anim.SetBool("isFiring", false);
+
+                }
+            }
+
+            if (!Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.LeftArrow))
+            {
+                anim.SetBool("isFiring", false);
+            }
+        }
+    }
 }
---
+++ Assets/Scripts/CameraBorder.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBorder : MonoBehaviour {
+
+    public Texture2D textureImage;
+    public Texture2D black;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI()
+    {
+        int barWidth = Screen.height / 74;
+
+        GUI.DrawTexture(new Rect(0, Screen.height / 3 - barWidth,  Screen.width, barWidth), black);
+
+        GUI.DrawTexture(new Rect(0, Screen.height / 3 * 2 + 1,     Screen.width, barWidth), black);
+
+        int world = WorldControlManager.enabledWorld;
+
+        Rect borderSize = new Rect(0, world * Screen.height / 3, Screen.width, Screen.height / 3);
+        GUI.DrawTexture(borderSize, textureImage);
+    }
+}
